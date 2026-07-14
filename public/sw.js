f115269f1526fa1a85950b7ef99bef6be65575f3
rodolfodelagracia-beep <rodolfo.delagracia@gmail.com>
2026-07-14 07:06:49 -0300

    Adiciona botão de hora extra no celular

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aponta-facil-v2'
+const CACHE = 'aponta-facil-v3'
 const ESSENTIAL = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png']
 
 self.addEventListener('install', (event) => {
